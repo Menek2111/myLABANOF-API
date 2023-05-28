c09ca0aa6de16a4b9ecd74cf7f00037fbd4d67d9
Menek2111 <bergami50@gmail.com>
2023-05-28 23:17:48 +0200

    fix get osso

---
+++ ftp/applabanof/data/getOssaIndividuoByDistretto.php
@@ -0,0 +1,63 @@
+<?php
+function cors()
+{
+    // Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+        // you want to allow, and if so:
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400'); // cache for 1 day
+    }
+
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            // may also be using PUT, PATCH, HEAD etc
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+        exit(0);
+    }
+}
+cors();
+
+// Change this to your connection info.
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'applabanof';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'my_applabanof';
+// Try and connect using the info above.
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if (mysqli_connect_errno()) {
+    // If there is an error with the connection, stop the script and display the error.
+    echo json_encode(array('response' => 'Failed to connect to MySQL: ' . mysqli_connect_error()));
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = json_decode(file_get_contents("php://input", true));
+
+    $ossaFlag = false;
+
+    $sql = "SELECT *, tipoOsso.nome FROM (osso INNER JOIN tipoOsso ON tipoOsso.id=osso.tipoOsso)  WHERE osso.individuo='" . $data->individuo . "';";
+    $result = $con->query($sql);
+    $ossaRows = array();
+    while ($r = $result->fetch_assoc()) {
+        $ossaRows[] = $r;
+        $ossaFlag = true;
+    }
+
+    if ($ossaFlag) {
+        echo json_encode(array('response' => 'success', 'results' => $ossaRows));
+    } else {
+        echo json_encode(array('response' => 'error', 'error' => "Could not insert record: " . mysqli_error($con) . " " . $sql));
+    }
+
+
+    $con->close();
+}
+?>
