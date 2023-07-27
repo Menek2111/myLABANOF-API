550daf7e34d3591f31352aae29f30b861cefc583
Menek2111 <bergami50@gmail.com>
2023-07-28 01:53:43 +0200

    aggiunta funzionalità

--- ftp/applabanof/data/delete/deleteTipoOsso.php
+++ ftp/applabanof/data/delete/deleteTipoOsso.php
@@ -45,7 +45,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = json_decode(file_get_contents("php://input", true));
 
     // $sql = "DELETE FROM `patologiaGenerale` WHERE id=" . $data->id;
-    $sql = "DELETE FROM traumaGenerale WHERE id=" . $data->id;
+    $sql = "DELETE FROM tipoOsso WHERE id=" . $data->id;
 
     if (mysqli_query($con, $sql)) {
         echo json_encode(array('response' => "success"));
